188b06ff28205647e1a75f05e131849acb2ae888
Никита Кот <nkot2340@gmail.com>
2023-04-21 22:09:41 +0300

    Катя-ООП-2-2

---
+++ lab-OOP-2-2-Katya/Program.cs
@@ -0,0 +1,64 @@
+﻿
+namespace lab_OOP_2_2_Katya
+{
+    class OddInMassiveException : Exception
+    {
+        protected Exception? exception; // ? - жопускает значение NULL. ТОЛЬКО С  ВЕРСИИ 9.0!
+        private string? message;
+
+        public OddInMassiveException() { }
+
+        public OddInMassiveException(string message) : base(message)
+        {
+
+        }
+        public OddInMassiveException(string message, Exception inner) : base(message, inner)
+        {
+            this.message = message;
+            inner = exception;
+        }
+    }
+
+    class TryException : OddInMassiveException
+    {
+        public TryException(int[] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    throw new OddInMassiveException($"В массиве присутствуют нечетные числа", exception);
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[2];
+
+            try
+            {
+                TryException exception = new TryException(array);
+            }
+            catch (OddInMassiveException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
